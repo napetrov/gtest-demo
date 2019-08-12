355789015fc7d39e2dedb14dc8eb9c7c35fc9fe7
GalyaZalesskaya <33455576+GalyaZalesskaya@users.noreply.github.com>
2019-08-12 15:15:09 +0300

    Update example.cpp

--- src/example.cpp
+++ src/example.cpp
@@ -7,6 +7,7 @@ double add_numbers(const double f1, const double f2)
 
 double subtract_numbers(const double f1, const double f2)
 {
+    std::cout>>"I'm substracting numbers"
     return f1 - f2;
 }
 
